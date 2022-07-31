430e13bed8dd30fbb5128d1482caafee7821c9a3
sedatcakir26 <sedatcakir@gmail.com>
2022-07-31 12:04:52 +0300

    Initial Commit

---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by Sedat Çakır on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
